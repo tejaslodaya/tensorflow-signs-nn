1707bbceb24ccdcc906dbd02c30795d79f3ed80d
Tejas Lodaya <tejas.lodaya1@gmail.com>
2017-10-17 23:19:09 +0530

    change number of epochs

--- tf_main.py
+++ tf_main.py
@@ -14,7 +14,7 @@ X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 X_train, Y_train, X_test, Y_test = preprocess_data(X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, num_classes)
 
 # Training the parameters
-parameters = model(X_train, Y_train, X_test, Y_test, num_epochs=100)
+parameters = model(X_train, Y_train, X_test, Y_test)
 
 #Predict using a sample image of test dataset
 predicted_class = predict(X_test[:,1:2], parameters, X_test.shape[0])
