cdeb364a633f4cacb2dc47dfba07279bf0f503ec
Tejas Lodaya <tejas.lodaya1@gmail.com>
2017-10-20 19:29:57 +0530

    add all datasets + convert to int64 + pickling the final parameters

--- tf_main.py
+++ tf_main.py
@@ -1,14 +1,44 @@
 import numpy as np
 from tf_utils import load_dataset, preprocess_data
 from tf_app_utils import model, predict
+import pickle
 
 np.random.seed(1)
 
 # Parameters
 num_classes = 6
 
-# Loading the dataset
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+X_train_orig_0, Y_train_orig_0, X_test_orig_0, Y_test_orig_0, classes = load_dataset(
+'train_signs.h5',
+'test_signs.h5')
+
+X_train_orig_1, Y_train_orig_1, X_test_orig_1, Y_test_orig_1, _ = load_dataset(
+'train_signs_1.h5',
+'test_signs_1.h5')
+
+X_train_orig_2, Y_train_orig_2, X_test_orig_2, Y_test_orig_2, _ = load_dataset(
+'train_signs_2.h5',
+'test_signs_2.h5')
+
+X_train_orig_3, Y_train_orig_3, X_test_orig_3, Y_test_orig_3, _ = load_dataset(
+'train_signs_3.h5',
+'test_signs_3.h5')
+
+X_train_orig = np.append(X_train_orig_0, X_train_orig_1, axis = 0)
+X_train_orig = np.append(X_train_orig, X_train_orig_2, axis = 0)
+X_train_orig = np.append(X_train_orig, X_train_orig_3, axis = 0)
+
+Y_train_orig = np.append(Y_train_orig_0, Y_train_orig_1, axis = 1)
+Y_train_orig = np.append(Y_train_orig, Y_train_orig_2, axis = 1)
+Y_train_orig = np.append(Y_train_orig, Y_train_orig_3, axis = 1)
+
+X_test_orig = np.append(X_test_orig_0, X_test_orig_1, axis = 0)
+X_test_orig = np.append(X_test_orig, X_test_orig_2, axis = 0)
+X_test_orig = np.append(X_test_orig, X_test_orig_3, axis = 0)
+
+Y_test_orig = np.append(Y_test_orig_0, Y_test_orig_1, axis = 1)
+Y_test_orig = np.append(Y_test_orig, Y_test_orig_2, axis = 1)
+Y_test_orig = np.append(Y_test_orig, Y_test_orig_3, axis = 1)
 
 # Cleaning the dataset
 X_train, Y_train, X_test, Y_test = preprocess_data(X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, num_classes)
@@ -16,5 +46,9 @@ X_train, Y_train, X_test, Y_test = preprocess_data(X_train_orig, Y_train_orig, X
 # Training the parameters
 parameters = model(X_train, Y_train, X_test, Y_test)
 
+# Store parameters
+output = open('best_parameters.pkl','wb')
+pickle.dump(parameters, output)
+
 #Predict using a sample image of test dataset
-predicted_class = predict(X_test[:,1:2], parameters, X_test.shape[0])
+#predicted_class = predict(X_test[:,1:2], parameters, X_test.shape[0])
--- tf_utils.py
+++ tf_utils.py
@@ -2,20 +2,21 @@ import h5py
 import numpy as np
 import math
 
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_signs.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/test_signs.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+def load_dataset(train_file, test_file):
+    train_dataset = h5py.File('datasets/' + train_file, "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
+    test_dataset = h5py.File('datasets/' + test_file, "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
+
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 def preprocess_data(X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, num_classes):
@@ -27,6 +28,10 @@ def preprocess_data(X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, num_cl
     # Normalize image vectors
     X_train = X_train_flatten / 255.
     X_test = X_test_flatten / 255.
+
+    # Converting np.bytes to np.int64
+    Y_train_orig = Y_train_orig.astype(np.int64)
+    Y_test_orig = Y_test_orig.astype(np.int64)
 
     # Convert training and test labels to one hot matrices
     Y_train = convert_to_one_hot(Y_train_orig, num_classes)
